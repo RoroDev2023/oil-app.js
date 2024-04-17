73602f7e20f821572ae9620392ef8325a2e08958
Rahim <rahim.askarov.2004@gmail.com>
2024-04-16 20:33:23 -0400

    Update

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -11,18 +11,18 @@ const FrontPage = () => {
         <div>
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl font-mono uppercase">Shipping.</h1>
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl font-mono uppercase">Logistics.</h1>
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl font-mono uppercase">Oil Trading.</h1>
+          <h1 className="mb-4 text-4xl font-extrabold leading-none text-white md:text-5xl lg:text-6xl font-mono uppercase tracking-tight">Oil<span className="ml-5 tracking-tighter">Trading.</span></h1>
           <p className="mb-8 text-lg font-normal text-gray-200 lg:text-xl sm:px-16 lg:px-48" style={{ padding: 0 }}>Welcome to True Holding LLC, your reliable partner in logistics, ship chartering, and oil trading.</p>
         </div>
         <div className='flex flex-row rounded-sm mt-48 hidden md:flex'>
           <div className="flex flex-col mr-8">
-            <Link to="/trading" className="bg-[#1E90FF] rounded-full py-1 px-2 cursor-pointer items-end">
+            <Link to="/trading" className="bg-[#1E90FF] rounded-full py-1 px-3 cursor-pointer">
               <span className="text-white text-lg font-bold">Trading</span>
               <FontAwesomeIcon className='text-black text-xl cursor-pointer content-center self-center ml-2' icon={faCircleArrowRight} />
             </Link>
           </div>
           <div className="flex flex-col items-center">
-            <Link to="/about" className="bg-[#1E90FF] rounded-full py-1 px-2 cursor-pointer">
+            <Link to="/about" className="bg-[#1E90FF] rounded-full py-1 px-3 cursor-pointer">
               <span className="text-white text-lg font-bold">About Us</span>
               <FontAwesomeIcon className='text-black text-xl cursor-pointer content-center self-center ml-2' icon={faCircleArrowRight} />
             </Link>
