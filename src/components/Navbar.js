f2bd7037ff277c843cd4acc5697f02532aea40c2
Rahim <rahim.askarov.2004@gmail.com>
2024-04-16 20:07:41 -0400

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,11 +15,11 @@ function Navbar() {
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">True Holding LLC</span>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">True Holding LLC</span>
           </Link>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">+971 58 567 1088</button>
-            <button onClick={toggleMenu} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-expanded={isMenuOpen ? 'true' : 'false'}>
+            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">+971 58 567 1088</button>
+            <button onClick={toggleMenu} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-expanded={isMenuOpen ? 'true' : 'false'}>
               <span className="sr-only">Open main menu</span>
               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
@@ -29,16 +29,16 @@ function Navbar() {
           <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMenuOpen ? 'block' : 'hidden'}`} id="navbar-sticky">
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
               <li>
-                <Link to="/" className={`block py-2 px-3 rounded ${location.pathname === '/' ? 'text-white bg-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent dark:text-white dark:hover:bg-gray-700'}`} aria-current={location.pathname === '/' ? 'page' : undefined}>Home</Link>
+                <Link to="/" className={`block py-2 px-3 rounded ${location.pathname === '/' ? 'text-white bg-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent text-white hover:bg-gray-700'}`} aria-current={location.pathname === '/' ? 'page' : undefined}>Home</Link>
               </li>
               <li>
-                <Link to="/trading" className={`block py-2 px-3 rounded ${location.pathname === '/trading' ? 'text-white bg-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent dark:text-white dark:hover:bg-gray-700'}`}>Trading</Link>
+                <Link to="/trading" className={`block py-2 px-3 rounded ${location.pathname === '/trading' ? 'text-white bg-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent text-white hover:bg-gray-700'}`}>Trading</Link>
               </li>
               <li>
-                <Link to="/about" className={`block py-2 px-3 rounded ${location.pathname === '/about' ? 'text-white bg-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent dark:text-white dark:hover:bg-gray-700'}`}>About Us</Link>
+                <Link to="/about" className={`block py-2 px-3 rounded ${location.pathname === '/about' ? 'text-white bg-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent text-white hover:bg-gray-700'}`}>About Us</Link>
               </li>
               <li>
-                <Link to="/contact" className={`block py-2 px-3 rounded ${location.pathname === '/contact' ? 'text-white bg-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent dark:text-white dark:hover:bg-gray-700'}`}>Contact Us</Link>
+                <Link to="/contact" className={`block py-2 px-3 rounded ${location.pathname === '/contact' ? 'text-white bg-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent text-white hover:bg-gray-700'}`}>Contact Us</Link>
               </li>
             </ul>
           </div>
